18887b85e3f9fe9680450d00023eb4bbf966804d
unknown <amado.adm@POAMOTANALY018.corp.eldorado.org.br>
2015-10-01 14:25:24 -0300

    Implemented an early version of the compare method. Implemented find neighbors method.

--- implementations/AIS/ais.py
+++ implementations/AIS/ais.py
@@ -2,13 +2,32 @@ import user as u
 import movieDB as mdb
 import random
 from sets import Set
+from operator import itemgetter, attrgetter, methodcaller
 
 
+#not used yet
+dList = []
 
 def main():
 	mdb.loadDB(mdb.FIXDB)
-	
-	generateRandomUsers(3,150)
+	users = generateRandomUsers(1500,150)
+	neighbors = findNeighbors(users, users[0], 10)
+	print neighbors
+
+#find k closest neighbors for mainUser in the list users
+def findNeighbors(users, mainUser, k):
+	distances = []
+	for user in users:
+		if user == mainUser: continue
+		distances.append([user,[compare(mainUser, user)]])
+	distances = sorted(distances, key=itemgetter(1))
+	#distances = distances[::-1]
+	neighbors = []
+	for i in range(k):
+		neighbors.append(distances.pop())
+	return neighbors
+	#print distances
+
 
 #generate random biased users
 def generateRandomUsers(nUsers, nMovies):
@@ -25,8 +44,9 @@ def generateRandomUsers(nUsers, nMovies):
 			score = biasScore(user, movie)
 			user.movieRatings.append((movie,score))
 		generatedUsers.append(user)
-		calculateGenreRate(user)
-	print generatedUsers
+#		calculateGenreRate(user)
+#	print generatedUsers
+	return generatedUsers
 
 #Give a score to a movie based on user genre bias
 def biasScore(user, movie):
@@ -58,7 +78,6 @@ def generateRandomGenreBias(user, n):
 
 #calculate how much an user likes each genre based on ratings
 def calculateGenreRate(user):
-	print '============='
 	genreOccurence = dict(mdb.countGenres(user.getMovieList()))
 	genreMap = dict()
 	for mt in user.movieRatings:
@@ -74,7 +93,36 @@ def calculateGenreRate(user):
 	genreRatio = dict()
 	for gen in genreMap.keys():
 		genreRatio[gen] = genreMap[gen]/(genreOccurence[gen] * len(user.movieRatings))
-		#print gen + ': ' + str(genreMap[gen]/(genreOccurence[gen] * len(user.movieRatings)))
-	mdb.printOrderedMap(genreRatio)
+	#mdb.printOrderedMap(genreRatio)
+	return genreRatio
+
+#function used to calculate correlation metric between two users
+#only using one pearson correlation so far
+def compare(user1,user2):
+	u1GenreRate =  calculateGenreRate(user1)
+	u2GenreRate =  calculateGenreRate(user2)
+	overlappedGenres = []
+	for genre1 in u1GenreRate.keys():
+		if genre1 in u2GenreRate.keys():
+			overlappedGenres.append(genre1)
+	sum1 = 0.0
+	for genre in overlappedGenres:
+		sum1 = sum1 + ( (u1GenreRate[genre] - averageOfVector(u1GenreRate.values())) * (u2GenreRate[genre] - averageOfVector(u2GenreRate.values())) )
+	sum2 = 0.0
+	for genre in overlappedGenres:
+		sum2 = sum2 + pow(( (u1GenreRate[genre] - averageOfVector(u1GenreRate.values()))),2)
+	sum3 = 0.0
+	for genre in overlappedGenres:
+		sum3 = sum3 + pow(( (u2GenreRate[genre] - averageOfVector(u2GenreRate.values()))),2)
+	pearson = sum1/pow((sum3*sum2),0.5)
+	return pearson
+
+#calculate the average value of a vector
+def averageOfVector(vector):
+	avg = 0
+	for x in vector:
+		avg = avg + x
+	return avg/len(vector)
+
 
 main()
--- implementations/AIS/user.py
+++ implementations/AIS/user.py
@@ -12,13 +12,16 @@ class User():
 		for m in self.movieRatings:
 			movieList.append(m[0])
 		return movieList
-	def compare(self,user):
-		#implement here a metric for correlation (spearman, pearson or a different one)
-		return 0
 	def __repr__(self):
-		print '======================='
-		print self.biasList
-		s = 'User: ' +  self.name + '\n'
+		#print '======================='
+		#print self.biasList
+		s = 'User: ' +  self.name 
 		#for movie in self.movieRatings:
 		#	s = s + movie[0]['Title'].encode('utf-8') + ' ' + str(movie[1]) + '  ' + movie[0]['Genre'].encode('utf-8') +'\n'
 		return s
+	def printUserInfo(self):
+		print '======================='
+		print self.biasList
+		print 'User: ' +  self.name 
+		for movie in self.movieRatings:
+			print movie[0]['Title'].encode('utf-8') + ' ' + str(movie[1]) + '  ' + movie[0]['Genre'].encode('utf-8') +'\n'
